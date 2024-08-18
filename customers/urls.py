8d065643d746027e1771d5862e359d1445728df1
Benedict Owusu <beneyjoy@gmail.com>
2024-08-18 09:30:35 +0000

    ability to serve

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import buy
+from .views import buy, view_queue, serve_buyer
 
 app_name = 'customers'
 
 urlpatterns = [
-    path('', buy, name='buy')
+    path('', buy, name='buy'),
+    path('list/', view_queue, name='requests'),
+    path('serve/', serve_buyer, name='serve'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import BuyForm
+from .models import Customers
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def buy(request):
@@ -11,3 +13,15 @@ def buy(request):
     else:
         customers = BuyForm()
     return render(request, 'buy.html', {"customers": customers})
+
+@login_required
+def serve_buyer(request):
+    buy = Customers.objects.order_by('request_time').first()
+    if buy:
+        buy.delete()
+    return redirect('customers:serve')
+
+@login_required
+def view_queue(request):
+    buyer = Customers.objects.order_by('request_time')
+    return render(request, 'queue.html', {'buyer':buyer})
